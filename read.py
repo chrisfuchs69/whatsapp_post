ea8c539176fe389c8e73dfe5822509090ccd2fd0
Christian Fuchs <christianfuchs.1@web.de>
2024-12-22 16:23:57 -0500

    an easy pattern now works

--- read.py
+++ read.py
@@ -1,7 +1,4 @@
 import re
-import emoji
-from collections import Counter
-import matplotlib.pyplot as plt
 
 # Chat file
 chat_file = 'chat.txt'
@@ -15,20 +12,29 @@ except OSError:
     print(f'File {chat_file} not found')
     exit()
 
+print(type(chat_data))
 # Regular expression to match WhatsApp chat format
-message_pattern = re.compile(r'(\[\d{2}.\d{2}.\d{2}, \d{2}:\d{2}:\d{2}\])')
-# message_pattern = re.compile(r'(\[\d{2}.\d{2}.\d{2}, \d{2}:\d{2}:\d{2}\]: \w+:)')
-# Dictionary to store messages
-user_messages = {}
+#prog = re.compile(pattern)
+#message_pattern = re.compile(r'\[(\d{2}\.\d{2}\.\d{2}), (\d{2}:\d{2}:\d{2})\] (\w+): (.+)')
+#result = prog.match(string)
 
-# Parse the chat data
-for line in chat_data:
-    match = message_pattern.match(line)
-    if match:
-        day, month, year, hour, minute, user = match.groups()
-        if user not in user_messages:
-            user_messages[user] = []
-        user_messages[user].append(message)
 
-# Print complete directory
-print(user_messages)
+# Kompiliertes Muster
+pattern = re.compile(r"I like to eat \w+")
+
+# Liste mit Strings
+list_of_strings = [
+    "I like to eat meat", 
+    "I don't like to eat meat", 
+    "I like to eat fish", 
+    "I don't like to eat fish"
+]
+
+# Extrahiere alle Treffer
+outcome = [pattern.match(x) for x in list_of_strings if pattern.match(x)]
+
+print(outcome)
+
+
+
+
